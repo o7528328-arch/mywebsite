b7e9d42d315136369fbf3ac84d010fe77d82be90
o7528328-arch <o7528328@gmail.com>
2026-01-19 22:00:28 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 
+const display =document.getElementById("display");
+
+function putondisplay(input){
+    display.value += input;
+}
+
+function cleardisplay(){
+ display.value = "";
+}
+
+function ans(){
+    try{
+     display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error"
+    }
+
+}
